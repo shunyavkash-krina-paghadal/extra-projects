52a639c3f1c4b2a6aef62a42a78ec8dca5db82d4
shunyavkash-krina-paghadal <krina.paghadal@shunyavkash.com>
2025-04-08 18:14:53 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,14 +18,3 @@ window.addEventListener("scroll", function () {
   }
 });
 
-const switchBtn = document.getElementById("switch");
-const navMenu = document.querySelector(".nav-menu");
-
-// Toggle Navigation on Click
-switchBtn.addEventListener("change", function () {
-  if (this.checked) {
-    navMenu.classList.add("active");
-  } else {
-    navMenu.classList.remove("active");
-  }
-});
