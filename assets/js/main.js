fc3c4712ef874d8246981668c767859c62df3b8b
shunyavkash-krina-paghadal <krina.paghadal@shunyavkash.com>
2025-04-09 17:54:50 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,48 @@ window.addEventListener("scroll", function () {
   }
 });
 
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {},
+
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.getElementById("menu-btn");
+  const menuOverlay = document.getElementById("menu-overlay");
+  const closeBtn = document.getElementById("close-btn");
+
+  menuBtn.addEventListener("click", function () {
+    menuOverlay.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", function () {
+    menuOverlay.classList.remove("active");
+  });
+});
